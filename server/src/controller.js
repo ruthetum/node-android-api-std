87dbfb75fc4f00c7e56d7a111ff84643d5465f68
Heedong Kim <ruthetum@gmail.com>
2020-08-20 16:11:29 +0900

    post method 설정

--- server/src/controller.js
+++ server/src/controller.js
@@ -6,6 +6,19 @@ export const whoami = (req, res) => {
     return res.json(mine);
 }
 
+export const me = (req, res) => {
+    var jsonData = "";
+
+    req.on('data', function(data) {
+        jsonData = JSON.parse(data);
+    });
+    req.on('end', function(){
+        console.log("name : " + jsonData.name);
+        console.log("age : " + jsonData.age);
+    });
+    res.end();
+}
+
 let mine = [
     {
         name: "heedong",
