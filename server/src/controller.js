cd9b1f1c1ebc1b350d958a08f91ac8bac38a36bf
Heedong Kim <ruthetum@gmail.com>
2020-08-20 14:04:14 +0900

    whoami get method 설정

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,3 +1,14 @@
 export const home = (req, res) => {
     res.sendfile('./src/home.html');
 };
+
+export const whoami = (req, res) => {
+    return res.json(mine);
+}
+
+let mine = [
+    {
+        name: "heedong",
+        age: 24
+    }
+]
--- server/src/router.js
+++ server/src/router.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {
-    home
+    home,
+    whoami
 } from "./controller";
 
 export const router = express.Router();
 
 router.get('/', home);
+
+router.get('/whoami', whoami);
